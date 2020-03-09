4b00448fb3f1a71644af8935d09363ea17388040
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-03-09 23:24:26 +0300

    Added required files for the task

--- exercises/rotten-tomatos/app.js
+++ exercises/rotten-tomatos/app.js
@@ -1,14 +1,3 @@
-console.log('Hello')
+const reviewer = require('./lib/reviewer')
 
-/**
- * Adding two numbers.
- *
- * @param {number} x Number.
- * @param {number} y Number.
- * @returns {number} Heh.
- */
-function add (x, y) {
-  return x + y
-}
-
-console.log(add(1, 2))
+reviewer.hello()
