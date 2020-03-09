47dc5e09bfee8e76f96b1b23185d4f6db7a92554
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-03-09 22:44:22 +0300

    Lint is set up

---
+++ exercises/rotten-tomatos/app.js
@@ -0,0 +1,14 @@
+console.log('Hello')
+
+/**
+ * Adding two numbers.
+ *
+ * @param {number} x Number.
+ * @param {number} y Number.
+ * @returns {number} Heh.
+ */
+function add (x, y) {
+  return x + y
+}
+
+console.log(add(1, 2))
