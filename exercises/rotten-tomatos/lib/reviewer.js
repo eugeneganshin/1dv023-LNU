d4844e68022047e2bed666e2d7d48b3189431b6e
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-03-10 23:45:21 +0300

    can parse JSON object to js object

--- exercises/rotten-tomatos/lib/reviewer.js
+++ exercises/rotten-tomatos/lib/reviewer.js
@@ -3,22 +3,31 @@
 const fs = require('fs')
 const xml2js = require('xml2js')
 
-const parser = new xml2js.Parser()
+// const parser = new xml2js.Parser()
 const { resolve } = require('path')
 
 const imdbPath = resolve('lib', 'movies', 'movies.json')
+const rottenTomatosPath = resolve('lib', 'movies', 'movies.xml')
 const readFile = (path, options) => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, options, (err, data) => {
       if (err) {
         reject(err)
       } else {
-        console.log(data)
+        console.log(typeof (data))
         resolve(data)
       }
     })
   })
 }
 
+const getAverage = async () => {
+  const parser = new xml2js.Parser()
+  const text = await readFile(imdbPath, 'utf-8')
+  const data = JSON.parse(text)
+  console.log(typeof (data))
+}
+
 readFile(imdbPath, 'utf-8')
+// getAverage()
 module.exports.readFile = readFile
