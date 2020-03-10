d2c99ad75341ba9ce5731dcf47ff75d9fba9e1b6
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-03-10 23:35:17 +0300

    managed to read json file

--- exercises/rotten-tomatos/lib/reviewer.js
+++ exercises/rotten-tomatos/lib/reviewer.js
@@ -1,5 +1,24 @@
-const hello = () => {
-  console.log('hello')
+'use strict'
+
+const fs = require('fs')
+const xml2js = require('xml2js')
+
+const parser = new xml2js.Parser()
+const { resolve } = require('path')
+
+const imdbPath = resolve('lib', 'movies', 'movies.json')
+const readFile = (path, options) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, options, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        console.log(data)
+        resolve(data)
+      }
+    })
+  })
 }
 
-module.exports.hello = hello
+readFile(imdbPath, 'utf-8')
+module.exports.readFile = readFile
