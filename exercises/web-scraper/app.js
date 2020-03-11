9f3b8e63f5bd794e3df649585e5ad4f4a067a4e9
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-03-11 21:10:37 +0300

    can querySelect nodes of html dom of provided links

--- exercises/web-scraper/app.js
+++ exercises/web-scraper/app.js
@@ -1,1 +1,35 @@
-console.log('Hey')
+// npm start https://nodejs.org/en/ https://developer.mozilla.org/en-US/
+// so here we need a function that would open both websotes and scrap it
+// and create a set of links
+
+// function scrape should take one parameter
+// map this parameter to get two urls
+
+const fetch = require('node-fetch')
+
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+
+const nodeLink = 'https://nodejs.org/en/'
+const mozillaLink = 'https://developer.mozilla.org/en-US/'
+const urls = ['https://nodejs.org/en/', 'https://developer.mozilla.org/en-US/']
+
+const getText = async url => {
+  try {
+    const response = await fetch(url)
+    const data = await response.text()
+    return data
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const getLinks = async text => {
+  const data = await text
+  const dom = new JSDOM(data)
+  console.log(dom.window.document.querySelector('p').textContent)
+}
+
+// extractLinks(urls)
+getLinks(getText(nodeLink))
+// commnet
