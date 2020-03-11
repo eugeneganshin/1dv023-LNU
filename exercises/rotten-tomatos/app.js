7717d2f3560f29755eb16e1c5bc4ef8268a2b4da
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-03-11 16:47:32 +0300

    Measured time of executing each funcitons

--- exercises/rotten-tomatos/app.js
+++ exercises/rotten-tomatos/app.js
@@ -4,16 +4,20 @@ const { getAverage } = require('./lib/reviewer')
 
 const solution = async () => {
   try {
+    console.time('path.resolve')
     const imdbPath = path.resolve(__dirname, 'lib', 'movies', 'movies.json')
     const rottenTomatosPath = path.resolve(__dirname, 'lib', 'movies', 'movies.xml')
-
+    console.timeEnd('path.resolve')
+    console.time('getAverage')
     const imdbPromise = getAverage(imdbPath)
     const rottenTomatosPromise = getAverage(rottenTomatosPath)
-
+    console.timeEnd('getAverage')
+    console.time('promiseAll')
     const [imdb, rottenTomatos] = await Promise.all([imdbPromise, rottenTomatosPromise])
     console.log('Average rating')
     console.log('IMDB: ' + imdb)
     console.log('Rotten tomators:' + rottenTomatos)
+    console.timeEnd('promiseAll')
   } catch (error) {
     console.error(error)
   }
