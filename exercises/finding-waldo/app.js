7596616de8a577eff9dc1acf3a42aa09223a7f56
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-03-13 21:45:30 +0300

    Exercise done. Can search for word in files and fire events

--- exercises/finding-waldo/app.js
+++ exercises/finding-waldo/app.js
@@ -1,1 +1,14 @@
-const path = require('path')
+const { TextCrawler } = require('./lib/find-waldo')
+
+const textCrawler = new TextCrawler(/waldo/i)
+
+textCrawler.on('found', (file, match) => console.log(`Matched ${match} in file ${file}`))
+textCrawler.on('error', error => console.error(`Error: ${error.message}`))
+
+textCrawler.addFile('data/file1.txt')
+  .addFile('data/file2.txt')
+  .addFile('data/file3.json')
+  .addFile('data/file4.json')
+  .addFile('data/file5.json')
+
+textCrawler.find()
--- exercises/finding-waldo/lib/find-waldo.js
+++ exercises/finding-waldo/lib/find-waldo.js
@@ -9,20 +9,55 @@ class TextCrawler extends EventEmitter {
     this.files = []
   }
 
-  // I need a fucntion that will take argument
-  // Push argument into array
-  addFile () {
-
+  /**
+   * Takes a path to the file and creates array of files.
+   *
+   * @param {string} file Path to the file.
+   * @returns {TextCrawler} The current instanse.
+   */
+  addFile (file) {
+    this.files.push(file)
+    return this
   }
 
-  // I need a function that will read each file
-  // While it reads the file it should have a callback
-  // That will try and find the matching word
-  // If the word is found then emit event found
+  /**
+   * Reads each file and finds the required word.
+   * If found, emits new event 'found'.
+   * If Error, emits new event 'error'.
+   *
+   * @returns {TextCrawler} The current instanse.
+   */
   find () {
-
+    this.files.forEach(async file => {
+      try {
+        const text = await fs.readFile(file, 'utf-8')
+        const matches = text.match(this.word)
+        if (matches) {
+          matches.forEach(match => {
+            this.emit('found', file, match)
+          })
+        }
+      } catch (error) {
+        this.emit('error', error)
+      }
+    })
+    return this
   }
 }
 
-const test = new TextCrawler()
-test.addFile()
+module.exports.TextCrawler = TextCrawler
+
+// this.files.map(async file => {
+//   try {
+//     const text = await fs.readFile(file, 'utf-8')
+//     const matches = text.match(this.word)
+//     if (matches) {
+//       matches.map(match => {
+//         this.emit('found', file, match)
+//       })
+//     }
+//   } catch (error) {
+//     this.emit('error', error)
+//   }
+// })
+// return this
