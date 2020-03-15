3c529e174276974db4c1250006f80ef1878aa481
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-03-15 20:30:52 +0300

    kinda understand how to serve client side js files now

---
+++ exercises/follow-the-route/public/js/default.js
@@ -0,0 +1,9 @@
+document.querySelector('#home-page').addEventListener('click', e => {
+  window.location.href = '/'
+})
+document.querySelector('#new-page').addEventListener('click', e => {
+  window.location.href = '/products/new'
+})
+document.querySelector('#products-page').addEventListener('click', e => {
+  window.location.href = '/products'
+})
--- exercises/follow-the-route/public/js/index.js
+++ exercises/follow-the-route/public/js/index.js
@@ -1,3 +1,11 @@
-document.querySelector('.middle').addEventListener('click', () => {
+document.querySelector('.left').addEventListener('click', () => {
   window.location.href = '/products'
 })
+document.querySelector('.right').addEventListener('click', () => {
+  window.location.href = '/products/new'
+})
+
+// document.querySelector('.header').addEventListener('click', () => {
+//   console.log('click')
+//   window.location.href('/')
+// })
