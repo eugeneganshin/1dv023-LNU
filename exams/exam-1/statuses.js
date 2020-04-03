9ae8ac76c35da188354c8069331b00edfff4f789
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-04-03 20:37:15 +0300

    Can get the result now but still need some bug fixes.

--- exams/exam-1/statuses.js
+++ exams/exam-1/statuses.js
@@ -148,20 +148,36 @@ const pathToFile = path.resolve('data', 'statuses.json')
 const url = 'http://vhost3.lnu.se:20080/weekend'
 
 const logic = (moviesStatus, cafeSlots) => {
-  console.log(moviesStatus)
-  console.log(cafeSlots)
+  // console.log(moviesStatus)
+  // console.log(cafeSlots)
   const movieslotSplit = cafeSlots.map(val => val.split(/(\d+)/))
+  // console.log(movieslotSplit)
+
   const splitNum = str => {
     const middle = Math.ceil(str.length / 2)
     const s1 = str.slice(0, middle)
     const s2 = str.slice(middle)
-    return s1 + '.' + s2
+    return s1 + '-' + s2
   }
-  const a = movieslotSplit.map(val => splitNum(val[1]))
+
+  const whatDay = str => {
+    if (str === 'fri') {
+      return 'Friday'
+    } else if (str === 'sat') {
+      return 'Saturday'
+    } else if (str === 'sun') {
+      return 'Sunday'
+    } else {
+      return 'Other Day'
+    }
+  }
+  const a = movieslotSplit.map(val => whatDay(val[0]) + ' ' + splitNum(val[1]))
+  const b = a.map(e => e.split(' '))
+  console.log(b)
   const movieSlot = []
   for (let i = 0; i < moviesStatus.length; i++) {
     for (let j = 0; j < a.length; j++) {
-      movieSlot.push((parseFloat(a[j]).toFixed(2) - parseInt(moviesStatus[i].time)).toFixed(2) + ' ' + moviesStatus[i].movie + ' ' + moviesStatus[i].time + ' ' + movieslotSplit[j])
+      movieSlot.push((parseFloat(b[j][1]).toFixed(2) - parseInt(moviesStatus[i].time)).toFixed(2) + ' ' + moviesStatus[i].movie + ' ' + moviesStatus[i].time + ' ' + a[j])
     }
   }
   console.log(movieSlot)
@@ -170,18 +186,18 @@ const logic = (moviesStatus, cafeSlots) => {
   }
   const movieName = el => {
     if (el[1] === '01') {
-      return ' The Flying Deuces' + el[2]
+      return `On ${el[3]} the movie "The Flying Deuces" starts at ${el[2]} and there is a free table between ${el[4]}`
     } else if (el[1] === '02') {
-      return ' Keep Your Seats, Please' + el[2]
+      return `On ${el[3]} the movie "Keep Your Seats, Please" starts at ${el[2]} and there is a free table between ${el[4]}`
     } else if (el[1] === '03') {
-      return ' A day at the races' + el[2]
+      return `On ${el[3]} the movie "A day at the races" starts at ${el[2]} and there is a free table between ${el[4]}`
     } else {
       return 'No such movie'
     }
   }
   const aqq = movieSlot.filter(IfMoreThanTwoHours).map(val => val.split(' '))
   console.log(aqq.map(movieName))
-  console.log(aqq)
+  // console.log(aqq)
 }
 
 const main = async (url) => {
